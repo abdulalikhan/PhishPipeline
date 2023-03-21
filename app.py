af50dac603c76650f6bcc2f7d244dae23c962aaa
abdulalikhan <alikhan@cocashcreators.com>
2023-03-21 08:22:38 +0500

    Added asset fix for inline css urls

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import subprocess
 from bs4 import BeautifulSoup
@@ -26,7 +27,8 @@ for form in forms:
     form["action"] = "/login"
 
 # Find all the assets (images, CSS, and JavaScript) in the HTML
-assets = soup.find_all(["img", "link"])
+assets = soup.find_all(["img", "link", "style"])
+style_tags = soup.find_all(lambda tag: tag.has_attr('style'))
 
 # Resolve the URLs of the assets
 for asset in assets:
@@ -34,6 +36,20 @@ for asset in assets:
         asset["src"] = urljoin(base_url, asset["src"])
     elif asset.has_attr("href"):
         asset["href"] = urljoin(base_url, asset["href"])
+
+# Resolve the URLs of the assets in the style attribute
+for style_tag in style_tags:
+    style = style_tag['style']
+    start_index = style.find('url(') + 4
+    end_index = style.find(')', start_index)
+    url_value = style[start_index:end_index]
+    if not url_value.startswith(('http://', 'https://', '//')):
+        url_value = urljoin(base_url, url_value.replace('\\', '/'))
+    if url_value.startswith('http:/'):
+        url_value = url_value.replace('http:/', 'http://')
+    if url_value.startswith('https:/'):
+        url_value = url_value.replace('https:/', 'https://')
+    style_tag['style'] = style[:start_index] + url_value + style[end_index:]
 
 # Save the updated HTML to a new file
 with open("build/templates/index.html", "w") as f:
